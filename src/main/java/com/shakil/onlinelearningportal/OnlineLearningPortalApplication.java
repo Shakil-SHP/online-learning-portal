f976b1fc67343f21baa24317c4e34f98ec756d8d
shakil hossain <shakilhossainofficial@protonmail.com>
2021-11-27 13:49:30 +0600

    Basic project setup and database configuration.

---
+++ src/main/java/com/shakil/onlinelearningportal/OnlineLearningPortalApplication.java
@@ -0,0 +1,13 @@
+package com.shakil.onlinelearningportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineLearningPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineLearningPortalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shakil/onlinelearningportal/OnlineLearningPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shakil.onlinelearningportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineLearningPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
